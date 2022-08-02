5b9b8a23a89e97e2ddfdd61fdd3bcf8eff5bbd05
KimbergerA <subscribefan@gmail.com>
2022-08-02 22:45:51 +0200

    basic drink service

---
+++ src/main/java/com/example/progmaticwaiter/service/DrinkService.java
@@ -0,0 +1,29 @@
+package com.example.progmaticwaiter.service;
+
+import com.example.progmaticwaiter.model.Drink;
+import com.example.progmaticwaiter.repository.DrinkRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class DrinkService {
+
+    private DrinkRepository drinkRepository;
+
+    public DrinkService(DrinkRepository drinkRepository) {
+        this.drinkRepository = drinkRepository;
+    }
+
+    public List<Drink> getAll() {
+        return new ArrayList<>((Collection) drinkRepository.findAll());
+    }
+
+    public Drink saveDrink(Drink drink) {
+        drinkRepository.save(drink);
+
+        return drink;
+    }
+}
